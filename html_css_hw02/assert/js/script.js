bfaf1ef49fc225dd906d2147b82fc3435e433b7a
Litvinov Mikhail <sid-4d@inbox.ru>
2017-03-16 05:48:59 +0300

    css home work - 2

---
+++ html_css_hw02/assert/js/script.js
@@ -0,0 +1,8 @@
+function site_about() {
+    document.write("Персональный сайт <br> <span>Константин Алексеевич Коровин</span>");
+}
+
+function time_now() {
+    var today= new Date();
+    document.write(today.toDateString());
+}
